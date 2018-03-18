f6b09e3d0425ec42631960afa92de370be66950d
arvin_zhou <arvin_zhou@askey.com>
2018-03-18 10:01:09 +0800

    Add Hello world 1

--- GitOperationProject/ViewController.swift
+++ GitOperationProject/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello")
+        print("Hello world 1")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
